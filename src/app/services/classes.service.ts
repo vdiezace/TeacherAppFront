222df0c231096a12cd9900e2d4ea6aa5cac450d2
vdiezace <victoriadiezacereda@gmail.com>
2023-06-16 21:53:54 +0100

    conectando el servicio classes con la petición del back classes

--- src/app/services/classes.service.ts
+++ src/app/services/classes.service.ts
@@ -1,9 +1,32 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { LoginTokenService } from './login-token.service';
+import { Class } from '../interfaces/class.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClassesService {
 
-  constructor() { }
+  private baseUrl_classes: string;
+  private baseUrl_studentClasses: string;
+
+
+  constructor(private httpClient: HttpClient, private loginTokenService: LoginTokenService) {
+    this.baseUrl_classes = "http://localhost:3000/api/classes"
+    this.baseUrl_studentClasses = "http://localhost:3000/api/students-classes";
+  }
+
+  getClassesByStudent(pStudentId: number) {
+    return firstValueFrom(this.httpClient.get<any>(`${this.baseUrl_studentClasses}/${pStudentId}`, this.loginTokenService.getTokenHeader()))
+  }
+
+  getBookedClassesByTeacherAndDate(pTeacherId: number, pDate: string) {
+    return firstValueFrom(this.httpClient.get<any>(`${this.baseUrl_classes}/teacher/teacher=${pTeacherId}&date=${pDate}`, this.loginTokenService.getTokenHeader()))
+  }
+
+  create(newClass: Class) {
+    return firstValueFrom(this.httpClient.post<Class>(this.baseUrl_classes, newClass))
+  }
 }
