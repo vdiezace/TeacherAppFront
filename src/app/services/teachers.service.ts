1af17893159db9fbf88a3847066e74cf505340a7
olgakh <ola_la_84@yahoo.com>
2023-06-17 00:43:15 +0200

    teacher service
    creando metodos

--- src/app/services/teachers.service.ts
+++ src/app/services/teachers.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { firstValueFrom } from 'rxjs';
+import { LoginTokenService } from './login-token.service';
+import { Teacher } from '../interfaces/teacher.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +11,43 @@ export class TeachersService {
 
   private baseUrl: string;
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient,
+    private loginTokenService: LoginTokenService) {
     this.baseUrl = 'http://localhost:3000/api/teachers'
   }
 
   getAllTeachers() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Authorization': localStorage.getItem('token')!
-      })
-    }
     return firstValueFrom(
-      this.httpClient.get<any>(`${this.baseUrl}`, httpOptions)
+      this.httpClient.get<any>(`${this.baseUrl}`, this.loginTokenService.getTokenHeader())
     )
   }
 
-  getTeachers() {
+  getTeacherById(pTeacherId: number) {
     return firstValueFrom(
-      this.httpClient.get(this.baseUrl));
+      this.httpClient.get<any>(`${this.baseUrl}/${pTeacherId}`));
+  }
+
+  getTeacherClassHours(pTeacherId: number) {
+    return firstValueFrom(
+      this.httpClient.get<any>(`${this.baseUrl}/hours/${pTeacherId}`)
+    );
+  }
+
+  getTeachersByFilters(pFilterId: number) {
+    return firstValueFrom(
+      this.httpClient.get<any>(`${this.baseUrl}/filters/${pFilterId}`)
+    );
+  }
+
+  createNewTeacher(newTeacher: Teacher) {
+    return firstValueFrom(
+      this.httpClient.post<Teacher>(`${this.baseUrl}`, newTeacher)
+    );
+  }
+
+  updateTeacher(pTeacherId: number) {
+    return firstValueFrom(
+      this.httpClient.put<any>(`${this.baseUrl}/${pTeacherId}`, this.loginTokenService.getTokenHeader())
+    );
   }
 }
