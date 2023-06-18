bc7497b358961a8d638cf046a7db4bd4dfb25ef6
Emilio <emigc7@gmail.com>
2023-06-18 15:11:00 +0200

    Variaciones en admin

--- src/app/components/admin-dashboard/students/students.component.ts
+++ src/app/components/admin-dashboard/students/students.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { StudentService } from './students.service';
+import { StudentsService } from 'src/app/services/students.service';
 
 @Component({
   selector: 'app-students',
@@ -11,17 +11,17 @@ export class StudentsComponent {
 
 
   students = [
-    { nombre: 'Juan', apellidos: 'Pérez', ubicacion: 'Madrid', edad: 20 },
-    { nombre: 'María', apellidos: 'López', ubicacion: 'Barcelona', edad: 22 },
-    { nombre: 'Pedro', apellidos: 'Gómez', ubicacion: 'Valencia', edad: 19 },];
+    { nombre: 'Juan', apellidos: 'Pérez',email:'juan@gmail.com', numero:650650650, ubicacion: 'Madrid', edad: 20 },
+    { nombre: 'María', apellidos: 'López', email:'Maria@gmail.com',  numero:650650651, ubicacion: 'Barcelona', edad: 22 },
+    { nombre: 'Pedro', apellidos: 'Gómez', email:'Pedro@gmail.com',  numero:650650652, ubicacion: 'Valencia', edad: 19 },];
 
   public userData : any;
 
-  constructor( private studentsservice: StudentService) {
+  constructor( private studentsservice: StudentsService) {
   }
 
   async ngOnInit() {
-    const response = await this.studentsservice.getSutdentsData();
+    const response = await this.studentsservice.getAllStudents();
     console.log(response);
     this.userData= response;
     if (this.userData){
